6f243a16d6bd3364a35c01ccc21b3bed6e603a26
Romain Brault <mail@romainbrault.com>
2024-12-08 10:41:27 +0100

    Initial commit: 🥇 set up Python project

---
+++ src/test_gh_linux_py39_test_open_source/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: © 2024 Romain Brault <mail@romainbrault.com>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+"""Top-level module."""
+
+from typing import Final
+
+from test_gh_linux_py39_test_open_source.package_metadata import __version__
+
+
+__all__: Final = ["__version__"]
+"""Public module attributes."""
